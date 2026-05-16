a8e84304cb61398dd668dc406b062036a649d712
will <will@mayas-evil-laptop>
2026-05-16 12:33:59 -0700

    update grabconfig.sh

--- grabconfig.sh
+++ grabconfig.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-grab() {
-	if [ $# == 1 ]; then
-		echo "grabbing ~/$1"
-		cp ~/$1 .
-	else
-		echo "grabbing ~/$1/$2"
-		mkdir -p $1
-		cp -r ~/$1/$2 -t $1
-	fi
-}
-if [ ! -d config ]; then
-	mkdir config
-fi
-cd config
-grab .config/emacs init.el
-grab .config/emacs extensions
-grab .config/i3    config
-grab .config/X11   xmodmap
-grab .bashrc
+cp ~/.Xresources ~/.bashrc ~/.config/X11/xmodmap ~/.config/emacs/init.el ~/.config/emacs/extensions/simpc-mode.el ./
+cp ~/.config/i3/config ./i3config
